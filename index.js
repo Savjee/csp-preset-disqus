029fdcf75723999d3d013376f31f36842daf6343
Xavier Decuyper <xavier.decuyper@gmail.com>
2021-04-27 19:42:25 +0200

    Added policy

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+modules.exports = {
+    "frame-src": [
+        "disqus.com",
+        "c.disquscdn.com", 
+    ],
+
+    "img-src": [
+        "c.disquscdn.com", 
+        "referrer.disqus.com",
+    ],
+
+    "script-src": [
+        "c.disquscdn.com",
+        "disqus.com", 
+    ],
+
+    "style-src": [
+        "c.disquscdn.com",
+    ],
+
+    "prefetch-src": [
+        "c.disquscdn.com",
+        "disqus.com",
+    ],
+};
+
